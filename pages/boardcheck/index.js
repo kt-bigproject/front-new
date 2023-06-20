9f46c11f11716144850edbf044326acac05a748c
RulePark <sabersavior@naver.com>
2023-06-20 22:00:38 +0900

    bpost 0620

--- pages/boardcheck/index.js
+++ pages/boardcheck/index.js
@@ -1,42 +1,11 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { useParams } from 'react-router-dom';
+import React from 'react';
 
-// 확인할 것 : id, title, created_at->날짜, user, body->내용
 const Check = () => {
-    const [checkList, setCheckList] = useState([]);
-
-    const checkBoard = async () => {
-        const abcd = await (await axios.get('http://127.0.0.1:8000/blog/blog/')).data;
-        setCheckList(abcd);
-        
-        const id = 1;
-        const filterList = abcd.filter(board => board.id === id);
-        console.log(filterList);
-    }
-    useEffect(() => {
-        checkBoard();
-      }, [])
-    
-      const {id} = useParams();
-      console.log(useParams());
-
-        return (
-            <table>
-                <tbody>
-                    {checkList.map(board => (
-                        <tr key={board.id}>
-                            <td>{board.user}</td>
-                            <td>{board.title}</td>
-                            <td>{board.body}</td>
-                            <td>{board.image}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        )
-        
-}
+  return (
+    <div>
+      게시판 상세보기가 표시됩니다.
+    </div>
+  );
+};
 
 export default Check;
---
+++ src/components/Board/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useRouter } from "next/router";
+import axios from 'axios';
+
+const Board = ({ title, user, body, image }) => {
+    const Router = useRouter();
+
+    const moveToback = () => {
+        Router.push('/bpost');
+      }
+
+    const moveToUpdate = () => {
+        Router.push('/update/' + id);
+    }
+
+    const deleteBoard = async () => {
+        if (window.confirm('게시글을 삭제하시겠습니까?')) {
+          await axios.delete(`http://127.0.0.1:8000/blog/blog/${id}`).then((res) => {
+            alert('삭제되었습니다.');
+            Router.push('/bpost');
+          });
+        }
+      }
+
+
+  return (
+    <div>
+        <div>
+            <h2>{title}</h2>
+            <h5>{user}</h5>
+            <hr />
+            <p>{body}</p>
+            <label>{image}</label>
+        </div>
+        <div>
+            <button onClick={moveToUpdate}>수정</button>
+            <button onClick={deleteBoard}>삭제</button>
+            <button onClick={moveToback}>뒤로</button>
+        </div>
+    </div>
+    
+  );
+};
+
+export default Board;
