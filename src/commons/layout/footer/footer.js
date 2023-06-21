6537ee13dafb2c152c2bc4360d7ec35c0a821530
gyumin <kkms9419@naver.com>
2023-06-22 03:13:10 +0900

    header. footer

--- src/commons/layout/footer/footer.js
+++ src/commons/layout/footer/footer.js
@@ -1,38 +1,40 @@
 import styles from './styles.module.css';
 import React from 'react';
-// import 'boxicons/css/boxicons.min.css';
+import 'boxicons/css/boxicons.min.css';
 
 
 export default function LayoutFooter() {
+
+  
+
   return (
-            <footer className={styles.wrap}>
-              <div className="block lg:flex py-12">
-                <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center mb-6 md:text-left">
-                </div>
-                <div className="w-full">
-                </div>
-              </div>
-          <div className="bg-gray-200">
-                <div className="py-4 px-6 flex flex-wrap flex-col sm:flex-row">
-                  <p className={styles.kt}>Copyright© KT 에이블스쿨 </p>
-                  <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                  <img src="/Logo.svg" className={styles.logo} width={200} height={50} />
-              <div className={styles.container}>  
-                <div className={styles.flex}>
-                  <a href="https://www.instagram.com" target="_blank">
-                      <i className='bx bxl-instagram' style={{ cursor: 'pointer', color: 'navy'}}></i>
-                      </a> 
-                  <a href="https://www.facebook.com" target="_blank">
-                      <i className='bx bxl-facebook' style={{ cursor: 'pointer', color: 'navy'}}></i>
-                      </a>
-                  <a href="https://www.twitter.com" target="_blank">
-                      <i className='bx bxl-twitter' style={{ cursor: 'pointer', color: 'navy'}}></i>
-                      </a>
-                  </div>
-              </div>  
-              </span>
-                </div>
+    <footer className={styles.footerwrap}>
+      <div className={styles.imageContainer}>
+      <img src="/Logo.svg" className={styles.logo} width={190} height={62} />
+        <div className={styles.iconcontainer}>
+          <a href="https://www.instagram.com" target="_blank">
+              <i className='bx bxl-instagram' style={{ color: 'navy' }}></i>
+            </a>  
+          <a href="https://www.facebook.com" target="_blank">
+            <i className='bx bxl-facebook' style={{ color: 'navy' }}></i></a>
+          <a href="https://www.twitter.com" target="_blank">
+            <i className='bx bxl-twitter' style={{ color: 'navy' }}></i></a>
           </div>
-        </footer>
-);
+        </div>
+      <div className={styles.footercontainer}>
+        <div className={styles.textContainer}>
+          <span className={styles.text}>
+            오늘의 손글씨에 오신 것을 환영합니다!<br/>
+            오늘의 손글씨는 만 13세 미만의 어린이와 외국인을 대상으로 손글씨 학습을 제공하는 사이트입니다.<br/> 
+            손글씨를 연습하고 개선하며 창의적인 작품을 만들어보세요.
+          </span>
+          <div className={styles.dividerContainer}> 
+          <hr className={styles.divider}/> 
+          </div>
+        </div>
+        <div>© KT 에이블스쿨. All rights reserved.</div>  
+        
+      </div>
+    </footer>  
+  );
 }
