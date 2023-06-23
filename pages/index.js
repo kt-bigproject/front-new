63e83394f9459eecaf2f22ccb140dead03a501b7
MUYEONKIM <rlaandus2@gmail.com>
2023-06-23 18:09:57 +0900

    #일요일까지 만들어오세요

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import {Banner, FeatureSection, FeatureSection2, FeatureSection3, FeatureSection4, ImageSpan, Main, MainBox, MainDiv, MainFeatures, MainImage, MainP, MenuBox} from '../styles/main/index'
-
+import Link from 'next/link';
+import {Footer, Banner, BannerDiv, EndBanner, EndBanner1, EndBanner2, FeatureSection, FeatureSection2, FeatureSection3, FeatureSection4, ImageSpan, Main, MainBox, MainDiv, MainFeatures, MainImage, MainP, MenuBox, FooterUl} from '../styles/main/index'
+import LayoutHeader from '../src/commons/layout/header/header';
 export default function MainPage() {
   return (
     <>
@@ -35,13 +36,16 @@ export default function MainPage() {
 				</header> */}
 
 			{/* Banner */}
+      <LayoutHeader />
 				<Banner>
-					<h1>오늘의 <br/>글씨</h1>
-					<p>Another fine responsive site template freebie by HTML5 UP.</p>
-					<div>
-            <button>a</button>
-            <button>b</button>
-					</div>
+          <BannerDiv>
+					  <h1>오늘의 <br/>글씨</h1>
+					  <p>Another fine responsive site template freebie by HTML5 UP.</p>
+					  <div>
+              <button>a</button>
+              <button>b</button>
+					  </div>
+          </BannerDiv>
 
 			{/* Main */}
 
@@ -56,9 +60,10 @@ export default function MainPage() {
 						</MainDiv>
 						<ImageSpan class="image featured"><MainImage src="/main/effect6.png" alt="" /></ImageSpan>
 					</MainBox>
+          </Main>
+          <EndBanner1 />
 
 					<MenuBox class="box special features">
-						<MainFeatures class="features-row">
 							<FeatureSection>
 								<span class="icon solid major fa-bolt accent2"></span>
 								<h3>Magna etiam</h3>
@@ -71,8 +76,6 @@ export default function MainPage() {
                 <br />
 								<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
 							</FeatureSection2>
-						</MainFeatures>
-						<MainFeatures class="features-row">
 							<FeatureSection3>
 								<span class="icon solid major fa-cloud accent4"></span>
 								<h3>Sed feugiat</h3>
@@ -85,12 +88,19 @@ export default function MainPage() {
                 <br />
 								<p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
 							</FeatureSection4>
-						</MainFeatures>
 					</MenuBox>
-
+          <button>DO IT DO IT!!!</button>
+          <EndBanner2 />
+          </Banner>
+          
 					<div class="row">
-						<div class="col-6 col-12-narrower">
-
+          <img
+          src="./main/pretty/giphy.gif"
+          width="384"
+          height="480"
+          frameBorder="0"
+          ></img>
+						{/* <div class="col-6 col-12-narrower">
 							<section class="box special">
 								<span class="image featured"><img src="images/pic02.jpg" alt="" /></span>
 								<h3>Sed lorem adipiscing</h3>
@@ -112,45 +122,28 @@ export default function MainPage() {
 								</ul>
 							</section>
 
-						</div>
+						</div> */}
 					</div>
 
-				</Main>
-        </Banner>
-
-			{/* CTA */}
-				<section id="cta">
-
-					<h2>Sign up for beta access</h2>
-					<p>Blandit varius ut praesent nascetur eu penatibus nisi risus faucibus nunc.</p>
-
-					<form>
-						<div class="row gtr-50 gtr-uniform">
-							<div class="col-8 col-12-mobilep">
-								<input type="email" name="email" id="email" placeholder="Email Address" />
-							</div>
-							<div class="col-4 col-12-mobilep">
-								<input type="submit" value="Sign Up" class="fit" />
-							</div>
-						</div>
-					</form>
-
-				</section>
-
 			{/* Footer */}
-				<footer id="footer">
-					<ul class="icons">
-						<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+				<Footer id="footer">
+					<FooterUl class="icons">
+						{/* <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
 						<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
 						<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
 						<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
 						<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
-						<li><a href="#" class="icon brands fa-google-plus"><span class="label">Google+</span></a></li>
-					</ul>
+						<li><a href="#" class="icon brands fa-google-plus"><span class="label">Google+</span></a></li> */}
+            <Link href="/"><a>Twitter</a></Link>
+            <Link href="/"><a>Google</a></Link>
+            <Link href="/"><a>Facebook</a></Link>
+            <Link href="/"><a>instagram</a></Link>
+            <Link href="/"><a>git</a></Link>
+          </FooterUl>
 					<ul class="copyright">
-						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						<div>&copy; Untitled. All rights reserved.</div>
 					</ul>
-				</footer>
+				</Footer>
 
 		</div>
 
