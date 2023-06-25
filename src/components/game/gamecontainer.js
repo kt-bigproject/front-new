739ad11faedbf873737f24079a0daf764f42627d
gyumin <kkms9419@naver.com>
2023-06-25 23:41:09 +0900

    practice, game page

--- src/components/game/gamecontainer.js
+++ src/components/game/gamecontainer.js
@@ -12,6 +12,7 @@ import { Alldiv, Mydiv2, Mydiv3, Mydiv4, Mydiv5, MyButton1, MyButton2 } from "..
 import { useRouter } from "next/router";
 import { useAxios } from "../Axios/axios";
 
+
 const Mycanvas = styled.canvas`
 border: 1px solid;
 background-image: url("/grid.png");
@@ -254,102 +255,160 @@ export default function Gamepage(props) {
   
       setFont(countToWord(count));
     }, []);
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+    };
+  
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+    };
+
+
+
+
   return(
     <>
-    <Alldiv>
-      <div style= {{textAlign:"center", marginTop:50}}>
-        {/* <img style = {{width: 250, height: 140}} src="/LOGO.png" /> */}
-        <ReadOutlined style={{fontSize:100, color:"#fa6400"}}/>
-      </div>
-      <div style= {{marginTop:20}}>
-        <button onClick={props.NextLevel}>이동하기</button>
-        <h2>글씨 놀이터</h2>
-      </div>
-      <div style= {{marginTop:20}}>
-        <h3>연습한 글씨체를 게임을 통해 직접 적어보세요! 블라블라 글씨체 다르게</h3>
-      </div>
-      <div>
-        <h4>여기에는 더더욱 부차적인 내용이 들어갈거에요 글씨체 다르게</h4>
-      </div>
-      <Mydiv2 style= {{marginTop:50}}>
-          <img style = {{width: 170, height: 170}} src="/left.png" />
-          <div style={MyDivStyle}>{props.sent}</div>
-          <img style = {{width: 170, height: 170}} src="/right.png" />
-      </Mydiv2>
-      <br />
-      <Mydiv>
-      <Mycanvas ref={canvasRef}
-                onMouseDown={startDrawing} // 마우스 버튼을 눌렀을때
-                onMouseUp={EndDrawing} // 마우스마우스 버튼을 땠을 때
-                onMouseMove={drawing} // 마우스가 움직일 때
-                onMouseLeave={EndDrawing} // 마우스가 캔버스를 벗어낫을 때
-      ></Mycanvas>
-      <Mydiv4>
-        <Button type="text" onClick={onClickClear}><DeleteOutlined /></Button>
-        <Button type="text" onClick={onClickEraser}><UndoOutlined /></Button>
-        <Button type="text" onClick={onClickPencil}><HighlightOutlined /></Button>
-      </Mydiv4>
-    </Mydiv>
-      <Mydiv5>
-      <MyButton1 size="default" onClick={onClickSubmit}><h4>손글씨 등록하기</h4></MyButton1>
-      <MyButton2 onClick={() => setOpen(curr => !curr)}><h4>사진 등록하기</h4></MyButton2>
-      <CustomModal open={open} setOpen={setOpen}>
-        <h1 style={{textAlign:"center"}}>서체를 사진으로 찍어 등록 해주세요!</h1>
-        <ImgUploadContainer>
-          <h2>사진을 업로드 하세요</h2>
-          <br />
-          <Upload
-              action="http://localhost:3000/"
-              listType="picture-card"
-              fileList={fileList}
-              onPreview={handlePreview}
-              onChange={handleChange}
-          >
-              {fileList.length >= 1 ? null : uploadButton}
-          </Upload>
-          <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
-              <img
-                  style={{
-                      width: '100%',
-                  }}
-                  src={previewImage}
-              />
-          </Modal>
-          <br />
-          {/*서버 제출 버튼*/}
-          <Button variant="primary" id='submit-btn' type='submit' onClick={handleApi}>Submit</Button>
-        </ImgUploadContainer >
-      </CustomModal>
-      <CustomModal open={gameOpen} setOpen={setgameOpen}>
-        <h1 style={{textAlign:"center"}}>당신의 점수는!</h1>
-        <div style={{fontSize:"30px", textAlign:"center", height:"300px", display:"flex", flexDirection:"column", justifyContent:"space-evenly"}}>
-        {isLoading ? (
-          <LoadingOutlined />
-        ) : (
-        <>
-        {score >= 0.8 ? (
-          count <= 14 ? (
-          <div style={{height: "250px", display: "flex", flexDirection:"column", justifyContent:"space-between", alignItems:"center"}}>
-          <h3>축하합니다</h3>{score*100}점입니다!!<br />
-          <MyButton1 onClick={props.NextLevel}>다음 단계</MyButton1>
+    <Alldiv style={{ margin: 0, display: "block"}}>
+        <div className="up2div" style={{ display: "flex", flexDirection: "row", 
+                                        justifyContent: "center", backgroundImage: "url('/wrap6.jpg')", 
+                                        width: "100%", margin: 0, height: 570}}>
+                 <div className="lef2div" style={{ display: "flex", flexDirection: "row", justifyContent: "center", 
+                                                  alignItems: "center"}}>
+                  <div className="imgdiv" style={{ position: "relative"}}>            
+                    <img style = {{width: 520, height: 520, transform: "rotate(6deg)"}} src="/gamei3.png" />   
+                    <img style = {{width: 270, height: 320, position: "absolute", top: 100, left: 125,
+                                    transform: "rotate(5deg)"}} src="/gamei1.png" />     
+
+
+
+                  </div>                                                    
+          <div style= {{textAlign:"center", marginTop:30}}>
+            {/* <img style = {{width: 250, height: 140}} src="/LOGO.png" /> */}
+            {/* <ReadOutlined style={{fontSize:100, color:"#fa6400"}}/> */}
           </div>
+
+                  </div>  
+                  <div className="rig2div" style={{ display: "flex", flexDirection: "column", 
+                                alignItems: "center", justifyContent: "center", gap: 30}}>
+                        <div vstyle= {{marginTop:10}}>
+                          <h2 style={{ fontSize:80, color: "#fa6400", 
+                                      fontFamily: "Kimjungchul"}}>『글씨 놀이터』</h2>
+                        </div>
+                          <h3 style={{ fontSize:25, fontFamily: "Kimjungchul" }} >연습한 글씨체를 게임을 통해 직접 적어보세요!</h3>
+                        <div className="text" style={{ display: "flex", flexDirection: "column", 
+                                                      alignItems: "center", justifyContent: "center", gap: 25}}>
+                          <h4 style={{ fontSize:25, fontFamily: "Kimjungchul" }} >단어와 문장을 듣고 발음을 연습할 수 있는 기능도 제공합니다. </h4>
+                            <h5 style={{ fontSize:25, fontFamily: "Kimjungchul" }} >언어 학습의 재미를 경험해보세요. </h5>
+                            <button onClick={props.NextLevel} 
+                                    style={{ fontFamily: "Kimjungchul", color: "white", 
+                                              backgroundColor: "#fa6400", border: "white", cursor: "pointer",
+                                              width: 120, height: 50, fontSize: 40, 
+                                              borderTopLeftRadius: "80px",
+                                              borderTopRightRadius: "100px",
+                                              borderBottomLeftRadius: "100px",
+                                              borderBottomRightRadius: "100px",
+                                              transform: isHovered ? "scale(1.1)" : "none",
+                                              transition: "transform 0.3s ease",
+                                              marginTop: 20, textAlign: "center"}}
+                                              onMouseEnter={handleMouseEnter}
+                                              onMouseLeave={handleMouseLeave}>start</button>                           
+                        </div>
+                          {/* <img style = {{width: 190, height: 120}} src="/g2.gif"/>  */}
+                      </div>
+                    </div>  
+          <div className="down2div" style={{ display: "flex", flexDirection: "column", 
+                                              alignItems: "center", justifySelf: "center", 
+                                              backgroundImage: "url('/gamed6.jpg')", width: "100%" }}>
+            <Mydiv2 style= {{marginTop:50}}>
+                <img style = {{width: 130, height: 130}} src="/left.png" />
+                <div style={MyDivStyle}>{props.sent}</div>
+                <img style = {{width: 130, height: 130}} src="/right.png" />
+            </Mydiv2>
+            <br />
+            <Mydiv>
+            <Mycanvas ref={canvasRef}
+                      onMouseDown={startDrawing} // 마우스 버튼을 눌렀을때
+                      onMouseUp={EndDrawing} // 마우스마우스 버튼을 땠을 때
+                      onMouseMove={drawing} // 마우스가 움직일 때
+                      onMouseLeave={EndDrawing} // 마우스가 캔버스를 벗어낫을 때
+            ></Mycanvas>
+            <Mydiv4>
+              <Button type="text" onClick={onClickClear}><DeleteOutlined /></Button>
+              <Button type="text" onClick={onClickEraser}><UndoOutlined /></Button>
+              <Button type="text" onClick={onClickPencil}><HighlightOutlined /></Button>
+            </Mydiv4>
+          </Mydiv>
+            <Mydiv5>
+                <div className="butdiv2" style={{ display: "flex", flexDirection: "row", 
+                                alignItems: "center", justifyContent: "center", 
+                                gap: 50}}>
+                  <MyButton1 size="default" onClick={onClickSubmit} style={{ backgroundColor: "#fa6400", 
+                                                                          width: 140, height: 40, marginBottom: 40}}>
+                    <span style={{ fontFamily: "Kimjungchul", fontSize: 15 }}>손글씨 등록하기</span></MyButton1>
+                  <MyButton2 onClick={() => setOpen(curr => !curr)} style={{ width: 140, height: 40, fontSize: 17, marginBottom: 40 }}>
+                    <span style={{ fontFamily: "Kimjungchul" }}>사진 등록하기</span></MyButton2>
+                </div>  
+            <CustomModal open={open} setOpen={setOpen}>
+              <h1 style={{textAlign:"center"}}>서체를 사진으로 찍어 등록 해주세요!</h1>
+              <ImgUploadContainer>
+                <h2>사진을 업로드 하세요</h2>
+                <br />
+                <Upload
+                    action="http://localhost:3000/"
+                    listType="picture-card"
+                    fileList={fileList}
+                    onPreview={handlePreview}
+                    onChange={handleChange}
+                >
+                    {fileList.length >= 1 ? null : uploadButton}
+                </Upload>
+                <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
+                    <img
+                        style={{
+                            width: '100%',
+                        }}
+                        src={previewImage}
+                    />
+                </Modal>
+                <br />
+                {/*서버 제출 버튼*/}
+                <Button variant="primary" id='submit-btn' type='submit' onClick={handleApi}>Submit</Button>
+              </ImgUploadContainer >
+            </CustomModal>
+            <CustomModal open={gameOpen} setOpen={setgameOpen}>
+              <h1 style={{textAlign:"center"}}>당신의 점수는!</h1>
+              <div style={{fontSize:"30px", textAlign:"center", height:"300px", display:"flex", flexDirection:"column", justifyContent:"space-evenly"}}>
+              {isLoading ? (
+                <LoadingOutlined />
               ) : (
+              <>
+              {score >= 0.8 ? (
+                count <= 14 ? (
                 <div style={{height: "250px", display: "flex", flexDirection:"column", justifyContent:"space-between", alignItems:"center"}}>
-                <h3>축하합니다</h3>{score*100}점입니다!!<br/><h6>게임이 종료되었습니다</h6>
-                <MyButton1 onClick={GoHome2}>게임 종료</MyButton1>
+                <h3>축하합니다</h3>{score*100}점입니다!!<br />
+                <MyButton1 onClick={props.NextLevel}>다음 단계</MyButton1>
                 </div>
-              )
-          ) : (
-            <div style={{height: "250px", display: "flex", flexDirection:"column", justifyContent:"space-between", alignItems:"center"}}>
-            <h3>점수가 기준에 도달하지 못하였습니다</h3>{score*100}점입니다..<br/><h6>게임이 종료되었습니다</h6>
-            <MyButton1 onClick={GoHome2}>게임 종료</MyButton1>
-            </div>
-        )}
-  </>
-)}
-          </div>
-      </CustomModal>
-      </Mydiv5>
+                    ) : (
+                      <div style={{height: "250px", display: "flex", flexDirection:"column", justifyContent:"space-between", alignItems:"center"}}>
+                      <h3>축하합니다</h3>{score*100}점입니다!!<br/><h6>게임이 종료되었습니다</h6>
+                      <MyButton1 onClick={GoHome2}>게임 종료</MyButton1>
+                      </div>
+                    )
+                ) : (
+                  <div style={{height: "250px", display: "flex", flexDirection:"column", justifyContent:"space-between", alignItems:"center"}}>
+                  <h3>점수가 기준에 도달하지 못하였습니다</h3>{score*100}점입니다..<br/><h6>게임이 종료되었습니다</h6>
+                  <MyButton1 onClick={GoHome2}>게임 종료</MyButton1>
+                  </div>
+              )}
+        </>
+      )}
+                </div>
+            </CustomModal>
+            </Mydiv5>
+            </div>  
     </Alldiv>
     </>
   )
