c4b2b6beb122f0182b93fd4a2deccf504471fae5
RulePark <sabersavior@naver.com>
2023-06-27 20:38:09 +0900

    bpost 0627 - 1

--- pages/bpost/ImgModal.js
+++ pages/bpost/ImgModal.js
@@ -8,12 +8,21 @@ import { useState, useEffect } from 'react';
 import { Router } from 'react-router-dom';
 import { useRouter } from 'next/router';
 import Board from '../update/[number]/index';
+import { useAxios } from '../../src/components/Axios/axios';
+import CommentDeleteblog from '../../src/components/bpost/commentDelete';
+// import ScrollView
 
 
 export default function ImgModal({open, onClose, figureInfo}) {
 
   console.log(figureInfo)
   const [like, setLike] = useState(true);
+  const [counter, setCounter] = useState(0);
+
+  const like_btn = () => {
+    setLike(like + 1);
+  };
+  
 
   const boxStyle = {
     position: 'absolute',
@@ -45,7 +54,105 @@ export default function ImgModal({open, onClose, figureInfo}) {
     Router.push(`/update/` + figureInfo.id )
   }
 
-  const [comments, setComments] = useState([]);
+  // const [comments, setComments] = useState([]); // 기존 데이터
+  const api = useAxios();
+  const [commentText, setCommentText] = useState('');
+  const [comment, setComment] = useState([]);
+
+  // 새로 추가한 데이터 ---------------------------------------
+
+  // useEffect(() => {
+  //   fetchComments();
+  // }, []);
+
+  // const fetchComments = async () => {
+  //   try {
+  //     const response = await api.get(`/blog/comment/?blog=${figureInfo.id}`);
+  //     setComments(response.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // const handleSaveComment = async () => {
+  //   try {
+  //     const response = await api.post(`/blog/comment/?blog=${figureInfo.id}`, {
+  //       text: commentText,
+  //     });
+  //     setComments([response.data]);
+  //     setCommentText('');
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // 아래는 댓글기능 구현 (시도)코드입니다.
+
+  const [data, setData] = useState([])
+  const [del, setDel] = useState(false)
+  const [showReplyInput, setShowReplyInput] = useState(false);
+  const [replyText, setReplyText] = useState('');
+  const [commentId, setCommentId] = useState('');
+
+  const handleReplyClick = (commentId) => {
+    setCommentId(commentId);
+    setShowReplyInput(!showReplyInput);
+  };
+
+  console.log(figureInfo.id)
+
+  useEffect(()=>{
+    api.get(`/blog/comment/?blog=${figureInfo.id}`)
+      .then(response => {
+        setData(response.data)
+    })
+  }, [setData, del] );
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    console.log(comment);
+    console.log(figureInfo.id);
+
+    const formData = new FormData();
+    formData.append('blog', figureInfo.id);
+    formData.append('comment', comment);
+
+    const response = await api.post('/blog/comment/', formData)
+    const data = await response.data;
+    console.log(data)
+    if ( response.status == 201 ) {
+      console.log(data)
+    } else {
+      console.log(response.status)
+    }
+    setData(prevData => [...prevData, data]);
+  };
+
+  const submitReply = async e => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('blog', figureInfo.id);
+    formData.append('comment', replyText);
+    formData.append('parent', commentId);
+
+    const response = await api.post('/font/comment/', formData)
+    const data = await response.data;
+    console.log(data)
+    if ( response.status == 201 ) {
+      console.log(data)
+    } else {
+      console.log(response.status)
+    }
+    setData(prevData => [...prevData, data]);
+
+    setCommentId('');
+    setReplyText('');
+    setShowReplyInput(false);
+  };
+
+  // --------------------------------------------------------
   
 
   return (
@@ -70,24 +177,79 @@ export default function ImgModal({open, onClose, figureInfo}) {
         
         <div className={styles.commentBox}>
           <div className={styles.input_wrapper}>          
-            <TextField sx={inputStyle} fullWidth label="댓글" variant="standard"/>
-            <div className={styles.SendBtn}>
-              <IconButton
-                onClick={() => {}}> 
-                <SendIcon/>
-              </IconButton> 
+            {/* <TextField 
+            sx={inputStyle} 
+            fullWidth 
+            label="댓글" 
+            variant="standard"
+            추가한 데이터
+          
+            value={commentText}
+            onChange={(e) => setCommentText(e.target.value)}
+
+            
+
+            -------------
+            /> */}
+            {/* 힘찬님 코드 추가 (아래) */}
+            <div >
+            <form onSubmit={handleSubmit} >
+            <input style={{ marginLeft: 10}} onChange={(e)=>{ setComment(e.target.value) }}/>
+            <div>
+            <IconButton type='summit'>
+              <SendIcon className={styles.commentbtn} />
+            </IconButton>
             </div>
+            </form>
+            </div>
+            <div style={{height: 250}}>
+              {/* <ScrollView */}
+                {
+                data.length > 0 &&
+                data.slice().reverse().map((comment) => (
+                  <div className={styles.commentdiv} key={comment.id}>
+                    <span>{comment.user} {comment.comment} {comment.created_at}</span>
+                    <span onClick={() => handleReplyClick(comment.id)} style={{cursor: 'pointer'}}>답글쓰기</span>
+                    <CommentDeleteblog id={comment.id} state={[del, setDel]}/>
+
+                    {/* {comment.replies.length > 0 && (
+                      <div>
+                        {comment.replies.map((reply) => (
+                          <Comment key={reply.id} comment={reply} />
+                        ))}
+                      </div>
+                    )} */}
+
+                    {showReplyInput && commentId === comment.id &&(
+                      <div>
+                        <form onSubmit={submitReply}>
+                          <input onChange={(e)=>{ setReplyText(e.target.value) }}/>
+                          <button type='submit'>전송</button>
+                        </form>
+                      </div>
+                    )}
+                  </div>
+                ))
+                //  : ''
+                    }
+              </div>
+            {/* 힘찬님 코드 추가 (위) */}
+
+            
           </div>
 
           <div className={styles.likeBtn}>
             
           <IconButton
-              onClick={() => setLike(!like)}>
+              onClick={like_btn}
+          >
+            <><FavoriteIcon className={styles.heart}/>{like}</>
+              {/* onClick={() => setLike(!like)}>
               { 
                 like
                 ? <><FavoriteIcon className={styles.heart}/> 112 </>
                 : <><FavoriteBorderIcon className={styles.heart}/> 111 </>
-              }
+              } */}
             </IconButton>             
           </div>
           
@@ -102,7 +264,15 @@ export default function ImgModal({open, onClose, figureInfo}) {
 
 
             {/* 댓글여기에 쓰기 */}
+            {/* 아래는 추가본 -----------------------------*/}
+            {/* {comments.map((comment) => (
+                <div key={comment.id}>{comment.text}</div>
+              ))} */}
 
+
+
+
+            {/* ------------ -----------------------------*/}
             
           </div>
             <div className={styles.button_position}>
